0ae725c931c668f40cdd92903d7b2ab955b274fd
sajeevah <sa.hasith@gmail.com>
2023-09-24 12:50:18 +0530

    clean up the code and create a store

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
-/* eslint-disable react/react-in-jsx-scope */
-/**
- * @format
- */
-
+import React from 'react';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import {store} from './src/app/store/store';
 import {Provider} from 'react-redux';
-import configureStore from './store/configureStore';
-
-const store = configureStore();
 
 const RNRedux = () => (
   <Provider store={store}>
