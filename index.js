61daef718132a5cade2f10a4b594bb1254dcd2c8
sajeevah <sa.hasith@gmail.com>
2023-09-24 15:10:53 +0530

    counter slider

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
-import {store} from './src/app/store/store';
-import {Provider} from 'react-redux';
+import { name as appName } from './app.json';
+import { store } from './src/app/store';
+import { Provider } from 'react-redux';
 
 const RNRedux = () => (
   <Provider store={store}>
