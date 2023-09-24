89dd62238fdbcc0704e1d62d14b0a5fe8a7f5759
sajeevah <sa.hasith@gmail.com>
2023-09-24 11:37:32 +0530

    not working

--- App.tsx
+++ App.tsx
@@ -1,31 +1,31 @@
-import React, {Component} from 'react';
+import * as React from 'react';
 import {Button, StyleSheet, Text, View} from 'react-native';
-class App extends Component {
-  state = {count: 0};
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import {changeCount} from './src/actions/counts';
 
+class App extends React.Component<any, any> {
   decrementCount() {
-    let {count} = this.state;
-    count--;
-    this.setState({
-      count,
-    });
+    console.log('sajeeva : ', this.props);
+    let {count, actions} = this.props;
+    count.count--;
+    // actions.changeCount(count.count);
   }
 
   incrementCount() {
-    let {count} = this.state;
-    count++;
-    this.setState({
-      count,
-    });
+    console.log('hasith : ', this.props);
+    let {count, actions} = this.props;
+    count.count++;
+    // actions.changeCount(count.count);
   }
 
   render() {
-    const {count} = this.state;
+    const {count} = this.props;
 
     return (
       <View styles={styles.container}>
         <Button title="increment" onPress={() => this.incrementCount()} />
-        <Text>{count}</Text>
+        <Text>{count.count}</Text>
         <Button title="decrement" onPress={() => this.decrementCount()} />
       </View>
     );
@@ -40,4 +40,12 @@ const styles = StyleSheet.create({
   },
 });
 
-export default App;
+const mapStateToProps = (state: any) => state.count;
+
+const ActionCreators = Object.assign({}, changeCount);
+
+const mapDispatchToProps = (dispatch: any) => ({
+  actions: bindActionCreators(ActionCreators, dispatch),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
